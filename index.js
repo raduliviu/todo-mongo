3cfe1e352b4ea7bcfa5d94dc46cc5087780a5001
Radu Baltag <baltag.radu@gmail.com>
2021-09-30 21:00:41 +0200

    dotenv pw

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const dotenv = require('dotenv').config()
 const { MongoClient, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://travelApp:shperlolz10@cluster0.ueghe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const password = process.env.MONGO_DB_PW_KEY
+const uri = `mongodb+srv://travelApp:${password}@cluster0.ueghe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri);
 
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+async function addTask(req, res) {
+    res.send("Not implemented yet")
+ }
+
+ async function readTask(req, res) {
+    res.send("Not implemented yet")
+ }
+ 
 
 express()
     .use(express.json())
@@ -12,4 +23,6 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
+    .post("/task", addTask)
+    .get("/task", readTask)
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
